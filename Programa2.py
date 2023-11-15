8fe831587512fd9112f5a8a2f52fc13470fd0db2
nicolasquijada18 <nicolasquijada18@gmail.com>
2023-11-15 08:35:33 -0300

    Probando ubicar indice de una lista solo con if

---
+++ Programa2.py
@@ -0,0 +1,24 @@
+lista = [10, 15, 20, 30, 35, 40]
+
+numero = 30
+indice = int
+
+if numero in lista:
+    if lista[0] == numero:
+        indice = 0
+    if lista [1] == numero:
+        indice = 1
+    if lista[2] == numero:
+        indice = 2
+    if lista[3] == numero:
+        indice = 3
+    if lista[4] == numero:
+        indice = 4
+    if lista[5] == numero:
+        indice = 5               
+    print(f"Su numero esta en la lista con indice {indice}")
+else:
+    print("Su numero no esta en la lista")
+    
+    
+
